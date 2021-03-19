2120809574c3f459db89af9a5025d6fc9d940a6c
Ploskiy Shar <minishi94@mail.ru>
2021-03-19 17:47:46 +0300

    added navbar

--- src/HOC/Layout/Layout.jsx
+++ src/HOC/Layout/Layout.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import Drawer from '../../components/Navigation/Drawer/Drawer';
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
 import classes from './Layout.module.css';
 
 class Layout extends Component {
+  state = {
+    menu: false,
+  };
+
+  menuToggleHandler = () => {
+    this.setState({
+      menu: !this.state.menu,
+    });
+  };
+
   render() {
     return (
       <div className={classes.Layout}>
+        <Drawer isOpen={this.state.menu} />
+
+        <MenuToggle
+          onToggle={this.menuToggleHandler}
+          isOpen={this.state.menu}
+        />
         <main>{this.props.children}</main>
       </div>
     );
