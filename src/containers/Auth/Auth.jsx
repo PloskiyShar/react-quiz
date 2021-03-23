7b086b8f5d7339e47bbf98ef8edc2f6a26b47425
Ploskiy Shar <minishi94@mail.ru>
2021-03-23 23:51:56 +0300

    Auth form in progress

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+import classes from './Auth.module.css';
 
 class Auth extends Component {
-  state = {};
+  logInHandler = () => {};
+  createAccountHandler = () => {};
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <h1>Auth</h1>;
+    return (
+      <div className={classes.Auth}>
+        <div>
+          <h1>Auth</h1>
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <Input label="Email" />
+            <Input label="Password" errorMessage="Incorrect password" />
+
+            <Button type="success" onClick={this.logInHandler}>
+              Log in
+            </Button>
+            <Button type="primary" onClick={this.createAccountHandler}>
+              Create account
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classes from './QuizCreator.module.css';
 
 class QuizCreator extends Component {
   state = {};
