7dce2faa8bf8b4e47d2ca1f13e8104d68d38c6f5
Ploskiy Shar <minishi94@mail.ru>
2021-03-26 23:51:07 +0300

    add validation to auth

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -2,12 +2,100 @@ import React, { Component } from 'react';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import classes from './Auth.module.css';
+import is from 'is_js';
 
 class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Type correct email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Password',
+        errorMessage: 'Type correct password',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
+
   logInHandler = () => {};
+
   createAccountHandler = () => {};
+
   submitHandler = (event) => {
     event.preventDefault();
+  };
+
+  validateControl(value, validation) {
+    if (!value) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid; // if isValid was false, it won't be true
+    }
+
+    if (validation.email) {
+      isValid = is.email(value) && isValid;
+    }
+
+    if (validation.minLength) {
+      isValid = value.length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  }
+
+  onChangeHandler = (event, controlName) => {
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+
+    formControls[controlName] = control; // update local state's copy
+
+    this.setState({
+      formControls,
+    });
+  };
+
+  renderInputs = () => {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          errorMessage={control.errorMessage}
+          shouldValidate={!!control.validation} // if this property exists, this line is equal true
+          onChange={(event) => this.onChangeHandler(event, controlName)}
+        />
+      );
+    });
   };
 
   render() {
@@ -16,9 +104,7 @@ class Auth extends Component {
         <div>
           <h1>Auth</h1>
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <Input label="Email" />
-            <Input label="Password" errorMessage="Incorrect password" />
-
+            {this.renderInputs()}
             <Button type="success" onClick={this.logInHandler}>
               Log in
             </Button>
